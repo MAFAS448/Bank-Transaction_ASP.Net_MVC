846283435c822f612e558b5174e680f4f80d95ea
MAFA448 <mafasmohamed48@gmail.com>
2022-12-14 11:02:34 +0530

    Add project files.

---
+++ Bank-Transaction/Migrations/20221213131206_Initial Create.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bank_Transaction.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    TransectionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountNumber = table.Column<string>(type: "nvarchar(16)", nullable: false),
+                    BeneficiaryName = table.Column<string>(type: "nvarchar(25)", nullable: false),
+                    BankName = table.Column<string>(type: "nvarchar(25)", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(15)", nullable: false),
+                    Amount = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.TransectionId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transactions");
+        }
+    }
+}
---
+++ Bank-Transaction/Models/Transaction.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bank_Transaction.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransectionId { get; set; }
+
+        [Column(TypeName = "nvarchar(16)")]
+        public string AccountNumber { get; set; }
+
+        [Column(TypeName = "nvarchar(25)")]
+        public string BeneficiaryName { get; set; }
+
+        [Column(TypeName = "nvarchar(25)")]
+        public string BankName { get; set; }
+
+        [Column(TypeName = "nvarchar(15)")]
+        public string Code { get; set; }
+        public int Amount { get; set; }
+        public DateTime Date { get; set; }  
+    }
+}
